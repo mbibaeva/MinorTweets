0237a709259e2c718c0aafa3231387b881128e52
mbibaeva <melisanushk@gmail.com>
2019-12-19 20:24:18 +0300

    Added main runner

---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask import render_template, request, redirect, url_for
+from collections import Counter, defaultdict, OrderedDict
+import json
+import re
+import itertools
+import http.client, urllib.request, urllib.parse, urllib.error, requests, base64
+import pandas as pd
+import db_handler
+import twitter_api
+
+app = Flask(__name__)
+#tools = handler.Tools()
+
+#@app.route('/', methods=['get'])
+#def index():
+#    return render_template('index.html')
+
+@app.route('/', methods = ['post', 'get'])
+def main():
+    return render_template('main.html')
+
+
+
+@app.route('/tatar', methods = ['post', 'get'])
+def display_tatar(language):
+    #df = pd.read_csv('all_data.csv', sep=';').fillna('')
+    #names = tools.display_all()
+
+    return render_template('tatar.html', tweets=[])
+
+
+if __name__ == '__main__':
+    app.run()
